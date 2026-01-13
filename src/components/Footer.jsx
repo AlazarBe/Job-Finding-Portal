1d322ee40969987c0238449b1c1903df3b8678c9
Alazar Bedada <lazarusbedada@gmail.com>
2026-01-13 10:30:51 +0300

    i try to add sth efsun

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,6 @@
 function Footer() {
-    return (
-        <div>
-            Footer component
-        </div>
-    )
+  return <div>Footer component</div>;
 }
+//i try to add some thing but ican't know what i have to add
 
-export default Footer
+export default Footer;
